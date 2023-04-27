c481a2976f7be6384997b6ad67841e3cd31cb3bf
Xillyk <xillyk@hotmail.com>
2023-04-27 22:24:39 +0700

    add routes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import Login from "./Login";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Layout from "./components/Layout";
+import Admin from "./components/Admin";
+import Editor from "./components/Editor";
+import Home from "./components/Home";
+import LinkPage from "./components/LinkPage";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
+import Register from "./components/Register";
+import Unauthorized from "./components/Unauthorized";
 
 function App() {
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* protected routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="editor" element={<Editor />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="lounge" element={<Lounge />} />
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
