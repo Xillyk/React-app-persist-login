e8a217f17041bc42f480d56c5ccdeefba67fc992
Xillyk <xillyk@hotmail.com>
2023-04-28 23:48:25 +0700

    implement persist checkbox

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,7 +15,7 @@ interface AccessTokenPayload {
 }
 
 const Login = () => {
-  const { setAuth } = useAuth();
+  const { setAuth, persist, setPersist } = useAuth();
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -84,6 +84,15 @@ const Login = () => {
       errRef.current?.focus();
     }
   };
+
+  const togglePersist = () => {
+    setPersist((prev: boolean) => !prev);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("persist", persist);
+  }, [persist]);
+
   return (
     <section>
       <p
@@ -115,6 +124,15 @@ const Login = () => {
           required
         />
         <button>Sign In</button>
+        <div className="persistCheck">
+          <input
+            type="checkbox"
+            id="persist"
+            onChange={togglePersist}
+            checked={persist}
+          />
+          <label htmlFor="persist">Trust This Device</label>
+        </div>
       </form>
       <p>
         Need an account?
