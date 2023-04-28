461eb967195a81d5244f888022553a9617766517
Xillyk <xillyk@hotmail.com>
2023-04-28 16:35:20 +0700

    create hook useRefreshToken

---
+++ src/hook/useRefreshToken.ts
@@ -0,0 +1,24 @@
+import axios from "@/api/axios";
+import useAuth from "./useAuth";
+
+const useRefreshToken = () => {
+  const { setAuth } = useAuth();
+
+  const refresh = async () => {
+    const response = await axios.get("/refresh", {
+      // send cookie with request
+      withCredentials: true,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    setAuth((prev: any) => {
+      console.log(JSON.stringify(prev));
+      console.log(response.data.accessToken);
+      return { ...prev, accessToken: response.data.accessToken };
+    });
+    return response.data.accessToken;
+  };
+
+  return refresh;
+};
+
+export default useRefreshToken;
