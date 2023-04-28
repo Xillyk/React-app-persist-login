d9a1dc719e7f27ce4da65bd16a2ea36604e134f8
Xillyk <xillyk@hotmail.com>
2023-04-28 23:01:21 +0700

    add roles data when refresh tk

--- src/hook/useRefreshToken.ts
+++ src/hook/useRefreshToken.ts
@@ -13,7 +13,11 @@ const useRefreshToken = () => {
     setAuth((prev: any) => {
       console.log(JSON.stringify(prev));
       console.log(response.data.accessToken);
-      return { ...prev, accessToken: response.data.accessToken };
+      return {
+        ...prev,
+        accessToken: response.data.accessToken,
+        roles: response.data.roles,
+      };
     });
     return response.data.accessToken;
   };
