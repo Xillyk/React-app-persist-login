7255a674d844b14b48c03a62e9b562dfc98e7bb8
Xillyk <xillyk@hotmail.com>
2023-04-28 17:01:41 +0700

    disable restrict mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,13 @@ import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </AuthProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
